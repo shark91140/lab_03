525c3f42a310707f56c67474c1119dcbc7d39ce7
shark91140 <shark91140@gmail.com>
2016-03-15 10:47:50 +0800

    複製發蠢版

--- app/src/main/java/com/example/studente3/lab_03/MainActivity.java
+++ app/src/main/java/com/example/studente3/lab_03/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.studente3.lab_03;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,53 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    private int mScroe = 0;
+    private int mScroe2 = 0;
+
+    public void reset(View view) {
+        mScroe = 0;
+        mScroe2 =0;
+        TextView scroeText = (TextView)findViewById(R.id.scroe);
+        scroeText.setText(String.valueOf(mScroe));
+        TextView scroeText2 = (TextView)findViewById(R.id.scroe2);
+        scroeText2.setText(String.valueOf(mScroe2));
+    }
+
+    public void onepoint(View view) {
+        TextView scroeText = (TextView)findViewById(R.id.scroe);
+
+        scroeText.setText(String.valueOf(++mScroe));
+
+    }
+
+    public void twopoint(View view) {
+        TextView scroeText = (TextView)findViewById(R.id.scroe);
+
+        scroeText.setText(String.valueOf(mScroe = mScroe + 2));
+    }
+
+    public void threepoint(View view) {
+        TextView scroeText = (TextView)findViewById(R.id.scroe);
+
+        scroeText.setText(String.valueOf(mScroe = mScroe+3));
+    }
+    public void onepoint2(View view) {
+        TextView scroeText = (TextView)findViewById(R.id.scroe2);
+
+        scroeText.setText(String.valueOf(++mScroe2));
+
+    }
+
+    public void twopoint2(View view) {
+        TextView scroeText = (TextView)findViewById(R.id.scroe2);
+
+        scroeText.setText(String.valueOf(mScroe2 = mScroe2+2));
+    }
+
+    public void threepoint2(View view) {
+        TextView scroeText = (TextView)findViewById(R.id.scroe2);
+
+        scroeText.setText(String.valueOf(mScroe2 = mScroe2+3));
     }
 }
